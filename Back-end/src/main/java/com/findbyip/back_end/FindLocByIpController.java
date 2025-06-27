0870fc9e5f065d492798ffc51a5473369b35879d
LeksoBugadze <lesksobugadze@gmail.com>
2025-06-27 14:04:52 +0400

    added .env variables and implemented them in my java server

--- Back-end/src/main/java/com/findbyip/back_end/FindLocByIpController.java
+++ Back-end/src/main/java/com/findbyip/back_end/FindLocByIpController.java
@@ -3,6 +3,7 @@ package com.findbyip.back_end;
 import java.util.Map;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,22 +13,36 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+
 
 @RestController
 @CrossOrigin(origins = "*")
 public class FindLocByIpController {
 
+    private final String apiKey;
+    private final String baseURL;
+
+    public FindLocByIpController(){
+        Dotenv dotenv=Dotenv.load();
+        this.baseURL=dotenv.get("BASE_URL");
+        this.apiKey=dotenv.get("API_KEY");
+    }
+
     @PostMapping("/")
     public ResponseEntity<Map<String,Object>> handlEntity(@RequestBody String req) {
-        ObjectMapper mapper=new ObjectMapper();
         Map<String, Object> response = new HashMap<>();
         
+        ObjectMapper mapper=new ObjectMapper();
         try {
 
             Request request = mapper.readValue(req, Request.class);
             String parsedJson=request.getReq();
 
-            response.put("location",request.getReq());
+            System.out.println("Key "+ apiKey);
+
+            response.put("location",parsedJson);
 
         } catch (JsonProcessingException e) {
             e.printStackTrace();
