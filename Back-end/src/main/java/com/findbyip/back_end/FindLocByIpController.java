0d88171e565949aaf0f2dda7605c9c7e792cff35
LeksoBugadze <lesksobugadze@gmail.com>
2025-07-03 00:30:15 +0400

    added get res

--- Back-end/src/main/java/com/findbyip/back_end/FindLocByIpController.java
+++ Back-end/src/main/java/com/findbyip/back_end/FindLocByIpController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -56,6 +57,11 @@ public class FindLocByIpController {
         }
 
         return result;
+    }
+
+    @GetMapping("/")
+    public String gEntity(){
+        return "Find by ip SERVER";
     }
 
     @PostMapping("/")
